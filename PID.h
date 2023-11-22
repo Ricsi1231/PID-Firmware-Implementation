b3e3aa83587420f1eb5fb838f33c4984a5370620
Richard <121430990+Ricsi1231@users.noreply.github.com>
2023-11-22 11:34:42 +0100

    Add files via upload

---
+++ PID.h
@@ -0,0 +1,31 @@
+#ifndef PID_PID_H_
+#define PID_PID_H_
+
+#include "stm32f4xx_hal.h"
+
+class PID
+{
+	private:
+	float tau;
+	float limMinInt = -255;
+	float limMaxInt = 255;
+	float T;
+	float integrator;
+	float prevError;
+	float differentiator;
+	float prevMeasurement;
+	float out;
+
+	public:
+	PID();
+	float Update(float setpoint, float measurement);
+	
+	public:
+	float Kp;
+	float Ki;
+	float Kd;
+	float limMin;
+	float limMax;
+};
+
+#endif 
